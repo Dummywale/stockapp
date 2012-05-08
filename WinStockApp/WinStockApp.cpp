415d61cfae189c9b1cd5d0d09cb25caa03f73f95
Amit Parpiyani <amit.parpiyani@geodesic.com>
2012-05-08 12:43:28 +0530

    working code with add stock

--- WinStockApp/WinStockApp.cpp
+++ WinStockApp/WinStockApp.cpp
@@ -13,11 +13,12 @@ HINSTANCE hInst;								// current instance
 TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
 TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
 char stockDelims []= "~";
+
 struct StocksData
 {
     char *szName;
     char *szCode;
-    float fPrice; 
+    char *szPrice; 
     char *szData;
 };
 // = (StocksData *) malloc(100 * sizeof(StocksData )); 
@@ -135,8 +136,9 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 }
 static size_t getStockValue(void *ptr, size_t size, size_t nmemb, void *data)
 {
-    StocksData *strData = (StocksData *)data;
-    strData[count].szData = (char *)ptr;
+  /* StocksData *strData= (StocksData *)data;
+    strData->szData = (char *)ptr;*/
+    stkData[count]->szData = (char *)ptr;
 	return size * nmemb;
 
 }
@@ -150,7 +152,7 @@ void getStockInfo()
     strcpy(szHostName,"");
     strcpy(szHostName,"http://download.finance.yahoo.com/d/quotes.csv?s=");;
     //char szStock = *stock;  
-    strcat(szHostName,stkData[count]->szCode);
+    strcat(szHostName,stkData[count]->szCode);  
     strcat(szHostName,"&f=sl1d1t1c1ohgv&e=.csv");
     //"http://finance.yahoo.com/aq/autoc?query=tata&region=US&lang=en-US&callback=YAHOO.util.ScriptNodeDataSource.callbacks"
     chunk = curl_slist_append(chunk, "Host: download.finance.yahoo.com");
@@ -160,10 +162,8 @@ void getStockInfo()
     curl_easy_setopt(curl,CURLOPT_URL,szHostName);
     curl_easy_setopt (curl, CURLOPT_USERAGENT, "Mozilla 2003, that coolish version"); 
     curl_easy_setopt (curl, CURLOPT_HTTPHEADER,chunk);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION,&getStockValue);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA,stkData[count]); 
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION,&getStockValue);    
     res = curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
 }
 
 //
@@ -179,14 +179,17 @@ void getStockInfo()
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int wmId, wmEvent;
+	 wchar_t *lpszStockRate;
+	 wchar_t *lpszStockName;
+	 wchar_t *lpszStockCode;
+	 FILE *pWatchList;
 	PAINTSTRUCT ps;
 	static int  cxChar, cxCaps, cyChar ;
 	HDC hdc;
 	TCHAR       szBuffer [10] ;
     TEXTMETRIC  tm ;
-    FILE *pWatchList; 
-    pWatchList = fopen("stocks.txt","rb");
-    long lSize;
+   
+    int lSize = 0;
     char * buffer;
     size_t result;
 	switch (message)
@@ -198,38 +201,54 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         cxChar = tm.tmAveCharWidth ;
         cxCaps = (tm.tmPitchAndFamily & 1 ? 3 : 2) * cxChar / 2 ;
         cyChar = tm.tmHeight + tm.tmExternalLeading ;
+        pWatchList = fopen("stocks.txt","r");
         if (pWatchList!=NULL) 
         {
-            fseek (pWatchList , 0 , SEEK_END);
-            lSize = ftell (pWatchList);
+            //fseek (pWatchList , 0 ,SEEK_END);
+            
+            char data[10000] = {'\0'};
+            int counter = 0;
+            while((data[counter++] = getc(pWatchList)) != EOF) {
+                fprintf(stdout, "%c", data[counter-1]);
+                lSize++;
+            }
+            //lSize = ftell (pWatchList);
+            //printf("%ld", lSize); 
             rewind (pWatchList);
             // allocate memory to contain the whole file:
-            buffer = (char*) malloc (sizeof(char)*(lSize-1));
-            // copy the file into the buffer:
-            result = fread (buffer,1,lSize-1,pWatchList);
-            fclose (pWatchList);
-            char *stock = NULL;
-            char *stockDetails = NULL;
-            stock = strtok( buffer, stockDelims );
-            int k = 0;
-
-            while( stock != NULL ) 
+            if(lSize> 0)
             {
-                printf( "result is \"%s\"\n", stock );
-                stkData[count] = (StocksData *)malloc(sizeof(StocksData));
-                stkData[count]->szName = stock;
-                stock = strtok( NULL, stockDelims );
-                stkData[count]->szCode = stock;
-                getStockInfo();
-                stock = strtok( NULL, stockDelims );
-                count++;
-
-                //stockDetails =strtok(stockData[count].szData,delims);
-                // stockData[count].szNmae = stockDetails;
-                // stockData[count].szCode = strtok(NULL,delims);
-
+                buffer = (char*) malloc (sizeof(char)*(lSize-1));
+                strcpy(buffer, "");
+                // copy the file into the buffer:
+                result = fread (buffer,1,lSize-1,pWatchList);
+                buffer[lSize-1] = '\0';
+                fclose (pWatchList);
+                char *stock = NULL;
+                char *stockDetails = NULL;
+                stock = strtok( buffer, stockDelims );
+                while( stock != NULL ) 
+                {
+                    stkData[count] = (StocksData *)malloc(sizeof(StocksData));
+                    stkData[count]->szName = stock;
+                    stock = strtok( NULL, stockDelims );
+                    stkData[count]->szCode = stock;
+                    getStockInfo();
+                    stock = strtok( NULL, stockDelims );
+                    count++;
+                }
+            }
+           
+            for (int x=0; x<count;x++)
+            {
+                char *price = NULL;
+                price = strtok(stkData[x]->szData,",");
+                price = strtok(NULL,",");
+                stkData[x]->szPrice = price;
 
             }
+            
+            // curl_easy_cleanup(curl);
 
         } 
           break;
@@ -255,26 +274,27 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	
 	case WM_PAINT:
         hdc = BeginPaint(hWnd, &ps);
+        
         // TODO: Add any drawing code here...
         //MessageBox(NULL,_T("Cancel Add"),_T("Cancel Stock"),IDYES);
-        for (int i = 0 ; i < count; i++)
+             lpszStockName = (wchar_t *)malloc(sizeof(wchar_t)* 100);
+            lpszStockCode = (wchar_t *)malloc(sizeof(wchar_t)* 10);
+            lpszStockRate = (wchar_t *)malloc(sizeof(wchar_t)* 10);
+            StrCpyW(lpszStockName, L"");
+             StrCpyW(lpszStockCode, L"");
+              StrCpyW(lpszStockRate, L"");
+        for (int n = 0 ; n < count; n++)
         {
-            
-            
-            TextOut (hdc, 0, cyChar * i,(LPWSTR)stkData[i]->szName,sizeof(stkData[i]->szName)) ;
-
-            TextOut (hdc, 22 * cxCaps, cyChar * i,      
-                 (LPWSTR)stkData[i]->szCode,
-                sizeof(stkData[i]->szCode)) ;
-
-            //SetTextAlign (hdc, TA_RIGHT | TA_TOP) ;
-
-            /*TextOut (hdc, 22 * cxCaps + 40 * cxChar, cyChar * i,
-                (LPWSTR)stkData[i]->szData,
-                 sizeof(stkData[i]->szName)) ;*/
-
-           // SetTextAlign (hdc, TA_LEFT | TA_TOP) ;
+            MultiByteToWideChar(CP_ACP, 0, stkData[n]->szName, -1, lpszStockName, strlen(stkData[n]->szName));
+            TextOut(hdc,0,cyChar * n,lpszStockName,strlen(stkData[n]->szName));
+            MultiByteToWideChar(CP_ACP, 0, stkData[n]->szCode, -1, lpszStockCode, strlen(stkData[n]->szCode));
+            TextOut(hdc, 40* cxCaps,cyChar * n,lpszStockCode,strlen(stkData[n]->szCode));
+            MultiByteToWideChar(CP_ACP, 0, stkData[n]->szPrice, -1, lpszStockRate, strlen(stkData[n]->szPrice));
+            TextOut(hdc, 75* cxCaps,cyChar * n,lpszStockRate,strlen(stkData[n]->szPrice));
         }
+      //  free(lpszStockName);
+      //   free(lpszStockCode);
+        //  free(lpszStockRate);
         EndPaint(hWnd, &ps);
         break;
 	case WM_DESTROY:
@@ -288,8 +308,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 static size_t write_data(void *ptr, size_t size, size_t nmemb, void *data)
 {
+	
 	FILE *writehere = (FILE *)data;
-	FILE *stockNicks = fopen("stocks.txt","ab");
+	
+	//FILE *writehere = fopen("stocks.txt","a");
 	array_list* results;
 	const char* from = (const char*) ptr;
 	struct json_object *new_obj;
@@ -309,13 +331,25 @@ static size_t write_data(void *ptr, size_t size, size_t nmemb, void *data)
 	new_obj = (struct json_object *)array_list_get_idx(results, 0);
 	nick_obj = json_object_object_get(new_obj, "symbol");
 	name_obj = json_object_object_get(new_obj, "name");
-	if (stockNicks != NULL)
+	//constant char * name = ;
+	//cons char *code = ;
+	
+	 stkData[count] = (StocksData *)malloc(sizeof(StocksData));
+	stkData[count]->szCode = (char*)json_object_to_json_string(nick_obj);
+	stkData[count]->szName = (char*)json_object_to_json_string(name_obj) ;
+	getStockInfo();
+	char *price = NULL;
+	price =strtok(stkData[count]->szData,",");
+	price =strtok(NULL,",");
+	stkData[count]->szPrice = price;
+	count++;
+	if (writehere != NULL)
 	{
-	    fprintf(stockNicks,"%s~%s~",json_object_to_json_string(name_obj),json_object_to_json_string(nick_obj));
-	    
-	    return size*nmemb;
+	    fprintf(writehere,"%s~%s~",json_object_to_json_string(name_obj),json_object_to_json_string(nick_obj));
+	    //fclose(writehere);
 	}
-	fclose(stockNicks);	
+   // fclose(writehere);
+   
 	return size*nmemb;
 }
 
@@ -330,7 +364,7 @@ BOOL CALLBACK  AddDlgProc (HWND hAddDlg,UINT message,WPARAM wParam,LPARAM lParam
 			CURL *curl;
 			CURLcode res;
 			curl = curl_easy_init();
-			FILE *ftpfile = fopen("JSON-resp.txt", "wb");;
+		
 			LPWSTR  szBuf;
 			szBuf = (LPWSTR)GlobalAlloc(GMEM_FIXED,sizeof(LPWSTR) *100);
 			LPWSTR szHost;
@@ -340,30 +374,32 @@ BOOL CALLBACK  AddDlgProc (HWND hAddDlg,UINT message,WPARAM wParam,LPARAM lParam
 			StrCatW(szHost, L"http://finance.yahoo.com/aq/autoc?query=");
 			StrCatW(szHost, szBuf);
 			StrCatW(szHost,L"&region=US&lang=en-US&callback=YAHOO.util.ScriptNodeDataSource.callbacks");
-
-			char *ansistr = (char*)malloc(sizeof(char)*100);
-			WideCharToMultiByte(CP_ACP, 0, (LPWSTR)szHost, -1,ansistr, 1000, NULL, NULL);
+			
+			char *getCodeUrl = (char*)malloc(sizeof(char)*200);
+			WideCharToMultiByte(CP_ACP, 0, (LPWSTR)szHost, -1,getCodeUrl, 1000, NULL, NULL);
 
 			if(curl)
 			{
 				struct curl_slist *chunk = NULL;
-				
+				FILE *stockFile =  fopen("stocks.txt", "a");
 				chunk = curl_slist_append(chunk, "Host: d.yimg.com");
 				chunk = curl_slist_append(chunk, "Accept-Language: en-US,en;q=0.8");
 				chunk = curl_slist_append(chunk, "Accept-Encoding: gzip,deflate,sdch");
 				chunk = curl_slist_append(chunk, "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3");
-				curl_easy_setopt(curl,CURLOPT_URL,ansistr);
+				curl_easy_setopt(curl,CURLOPT_URL,getCodeUrl);
 		
 				curl_easy_setopt (curl, CURLOPT_REFERER, "http://finance.yahoo.com/"); 
 				curl_easy_setopt (curl, CURLOPT_USERAGENT, "Mozilla 2003, that coolish version"); 
 				curl_easy_setopt (curl, CURLOPT_HTTPHEADER,chunk);				
-				curl_easy_setopt(curl, CURLOPT_WRITEDATA, ftpfile); 
+				curl_easy_setopt(curl, CURLOPT_WRITEDATA,stockFile); 
 				curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &write_data); 
 				res = curl_easy_perform(curl);
 				curl_easy_cleanup(curl);
+				free(getCodeUrl);
+				fclose(stockFile);
 			}
 			
-			fclose(ftpfile);
+			
 			HWND hParentHandle;
 			//hParentHandle = GetParent(hAddDlg);
 			//InvalidateRect(hParentHandle,NULL,false);
